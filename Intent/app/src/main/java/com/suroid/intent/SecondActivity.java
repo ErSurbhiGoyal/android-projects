41a91844f9519e0ca1bd9e6d7826fc86fb617f40
unknown <surbhigoyal183@gmail.com>
2018-09-03 16:23:53 +0530

    Intent

---
+++ Intent/app/src/main/java/com/suroid/intent/SecondActivity.java
@@ -0,0 +1,16 @@
+package com.suroid.intent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        //TODO: Do whatever task you want to do on this new activity
+
+    }
+}
