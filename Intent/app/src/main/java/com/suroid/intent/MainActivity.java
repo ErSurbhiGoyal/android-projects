b7ed2202f18a7ecb7991875b728b66993e1376b8
surbhi <surbhi.goyal@indepay.com>
2019-03-07 18:45:56 +0530

    Removing unused imports

--- Intent/app/src/main/java/com/suroid/intent/MainActivity.java
+++ Intent/app/src/main/java/com/suroid/intent/MainActivity.java
@@ -2,7 +2,6 @@ package com.suroid.intent;
 
 import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -10,13 +9,12 @@ import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
--- Intent/app/src/main/java/com/suroid/intent/SecondActivity.java
+++ Intent/app/src/main/java/com/suroid/intent/SecondActivity.java
@@ -1,12 +1,7 @@
 package com.suroid.intent;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
-import butterknife.ButterKnife;
-
-import butterknife.InjectView;
+import android.support.v7.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
 
